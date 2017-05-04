fb9ef3bf1db0ff10e6a61993445d883689f47884
Andrew Lampinen <lampinen@stanford.edu>
2017-05-04 16:09:54 -0700

    adjusting for exploring_network_size naming conventions

--- hinton_family_tree/analogy_exploration.m
+++ hinton_family_tree/analogy_exploration.m
@@ -11,7 +11,7 @@ imagesc(Q1)
 nhidden = 1000;
 eta = 0.000949;
 weightsize = 2.0;
-run = 3;
+run = 8;
 
 single_l1_reps = [load(sprintf('results/simul_learning_3layer_single_inputs/hinton_batch_nhidden_%i_eta_%f_momentum_0.000000_weightsize_%f_rseed_%i_f1_single_input_pre_middle_reps.csv',nhidden,eta,weightsize,run-1)); load(sprintf('results/simul_learning_3layer_single_inputs/hinton_batch_nhidden_%i_eta_%f_momentum_0.000000_weightsize_%f_rseed_%i_f2_single_input_pre_middle_reps.csv',nhidden,eta,weightsize,run-1))];
 l1_reps = max(single_l1_reps,0);
@@ -32,6 +32,8 @@ l1_reps = max(actual_l1_reps,0);
 figure
 Q1 = squareform(pdist(l1_reps));
 imagesc(Q1)
+
+%% network size runs
 
 %% SVD stuff
 input = load('hinton_x_data.csv');
